fba0b29c7208d6dd2f7e16bd13e3b087d84f9941
Yash Sharma <yash.sharma2484@gmail.com>
2026-08-17 21:45:39 +0530

    first commit

---
+++ kitchen-dispatch/src/main/java/com/project/kitchen_dispatch/KitchenDispatchApplication.java
@@ -0,0 +1,13 @@
+package com.project.kitchen_dispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenDispatchApplication.class, args);
+	}
+
+}
---
+++ kitchen-dispatch/src/test/java/com/project/kitchen_dispatch/KitchenDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kitchen_dispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenDispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
