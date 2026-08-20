d8b746ce636d97f4a2d4fdb3a58b4cc0d4c70d8d
Yash Sharma <yash.sharma2484@gmail.com>
2026-08-20 10:32:48 +0530

    Update IDispatchService.java

--- kitchen-dispatch/src/main/java/com/project/kitchen_dispatch/service/interfac/IDispatchService.java
+++ kitchen-dispatch/src/main/java/com/project/kitchen_dispatch/service/interfac/IDispatchService.java
@@ -1,10 +1,13 @@
 package com.project.kitchen_dispatch.service.interfac;
 
 import com.project.kitchen_dispatch.model.Dispatch;
+import com.project.kitchen_dispatch.model.Order;
 
 public interface IDispatchService {
 
     Dispatch createDispatch(Dispatch dispatch);
 
     Dispatch getDispatchById(Long id);
+
+    Dispatch automaticallyDispatchOrder(Order order);
 }
